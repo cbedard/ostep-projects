b51e894b831a8a79db1666da63db968170b98d5c
Cameron Bedard <cam.bedardca@gmail.com>
2024-05-06 23:48:31 -0400

    str split

--- processes-shell/wish.c
+++ processes-shell/wish.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-char *getLine() {
+char *get_line() {
     char *line = NULL;
     size_t len = 0;
     ssize_t lineSize = 0;
@@ -12,7 +12,49 @@ char *getLine() {
     lineSize = getline(&line, &len, stdin);
     
     return line;
-    //free(line);
+}
+
+char** str_split(char* a_str, const char a_delim) {
+    char** result    = 0;
+    size_t count     = 0;
+    char* tmp        = a_str;
+    char* last_comma = 0;
+    char delim[2];  
+    delim[0] = a_delim;
+    delim[1] = 0;
+
+    // Count how many elements will be extracted.
+    while (*tmp) {
+        if (a_delim == *tmp) {
+            count++;
+            last_comma = tmp;
+        }
+        tmp++;
+    }
+
+    // Add space for trailing token.
+    count += last_comma < (a_str + strlen(a_str) - 1);
+
+    /* Add space for terminating null string so caller
+       knows where the list of returned strings ends. */
+    count++;
+
+    result = malloc(sizeof(char*) * count);
+
+    if (result) {
+        size_t idx  = 0;
+        char* token = strtok(a_str, delim);
+
+        while (token) {
+            assert(idx < count);
+            *(result + idx++) = strdup(token);
+            token = strtok(0, delim);
+        }
+        assert(idx == count - 1);
+        *(result + idx) = 0;
+    }
+
+    return result;
 }
 
 int main(int argc, char *argv[]) {
@@ -22,9 +64,10 @@ int main(int argc, char *argv[]) {
 
     do {
         printf("wish> ");
-        line = getLine();
-        //args = lsh_split_line(line);
-        //status = lsh_execute(args);
+        line = get_line();
+        args = str_split(line, " ");
+        
+        //status = execute(args);
 
         printf("You entered: \"%s\"", line); 
         free(line);
